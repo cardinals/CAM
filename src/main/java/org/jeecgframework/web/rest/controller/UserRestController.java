6f82cd62f740aae15b92342746162125a0cd9ebc
gavin666 <Gavin.Gao@e-bao.cn>
2016-03-04 16:19:03 +0800

    rest接口  /user   /user/id
    get接口基本没有问题，其他接口正在研究

--- src/main/java/org/jeecgframework/web/rest/controller/UserRestController.java
+++ src/main/java/org/jeecgframework/web/rest/controller/UserRestController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.util.UriComponentsBuilder;
 
 /**
- * TSUser的Restful API的Controller.  请求路径 rest/user
+ * TSUser的Restful API的Controller. 请求路径 rest/user
  * 
  * @author liuht
  */
@@ -40,61 +40,59 @@ public class UserRestController {
 	@Autowired
 	private Validator validator;
 
+	//get User 列表     路径  /rest/user
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
 	public List<TSUser> list() {
-		
-		List<TSUser> listUsers=userService.getList(TSUser.class);
+		List<TSUser> listUsers = userService.getList(TSUser.class);
 		return listUsers;
 	}
-	
+
+	//get User 列表     路径  /rest/user/id  
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<?> get(@PathVariable("id") Long id) {
-		
-		TSUser task = userService.get(TSUser.class, id);
-		if (task == null) {
+	public ResponseEntity<?> get(@PathVariable("id") String id) {
+		TSUser user = userService.get(TSUser.class, id);
+		if (user == null) {
 			return new ResponseEntity(HttpStatus.NOT_FOUND);
 		}
-		return new ResponseEntity(task, HttpStatus.OK);
+		return new ResponseEntity(user, HttpStatus.OK);
 	}
 
+	//post 保存用户  
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public ResponseEntity<?> create(@RequestBody TSUser user, UriComponentsBuilder uriBuilder) {
-		//调用JSR303 Bean Validator进行校验，如果出错返回含400错误码及json格式的错误信息.
+		// 调用JSR303 Bean Validator进行校验，如果出错返回含400错误码及json格式的错误信息.
 		Set<ConstraintViolation<TSUser>> failures = validator.validate(user);
 		if (!failures.isEmpty()) {
 			return new ResponseEntity(BeanValidators.extractPropertyAndMessage(failures), HttpStatus.BAD_REQUEST);
 		}
-
-		//保存用户
+		// 保存用户
 		userService.save(user);
-
-		//按照Restful风格约定，创建指向新任务的url, 也可以直接返回id或对象.
+		// 按照Restful风格约定，创建指向新任务的url, 也可以直接返回id或对象.
 		String id = user.getId();
 		URI uri = uriBuilder.path("/rest/user/" + id).build().toUri();
 		HttpHeaders headers = new HttpHeaders();
 		headers.setLocation(uri);
-
 		return new ResponseEntity(headers, HttpStatus.CREATED);
 	}
 
+	//更新用户
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> update(@RequestBody TSUser user) {
-		//调用JSR303 Bean Validator进行校验，如果出错返回含400错误码及json格式的错误信息.
+		// 调用JSR303 Bean Validator进行校验，如果出错返回含400错误码及json格式的错误信息.
 		Set<ConstraintViolation<TSUser>> failures = validator.validate(user);
 		if (!failures.isEmpty()) {
 			return new ResponseEntity(BeanValidators.extractPropertyAndMessage(failures), HttpStatus.BAD_REQUEST);
 		}
-
-		//保存
+		// 保存
 		userService.saveOrUpdate(user);
-
-		//按Restful约定，返回204状态码, 无内容. 也可以返回200状态码.
+		// 按Restful约定，返回204状态码, 无内容. 也可以返回200状态码.
 		return new ResponseEntity(HttpStatus.NO_CONTENT);
 	}
 
+	//删除用户
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable("id") Long id) {
